fc084c120c3de9ef6fd69f513bad849ed4192632
Esha Mitra <72601896+esha20@users.noreply.github.com>
2022-05-14 14:36:00 +0530

    Time: 19 ms (81.36%), Space: 5.9 MB (73.74%) - LeetHub

---
+++ 326-power-of-three/326-power-of-three.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n==3 || n==1)
+            return true;
+        else if (n==0 || n%3 != 0)
+            return false;
+        else
+            return isPowerOfThree(n/3);
+    }
+};
