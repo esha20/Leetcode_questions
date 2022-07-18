b423b77e66ee4dea23103b5fab7be0d015d8874d
Esha Mitra <72601896+esha20@users.noreply.github.com>
2022-07-18 15:55:03 +0530

    Time: 18 ms (20.07%), Space: 11.8 MB (54.75%) - LeetHub

---
+++ 61-rotate-list/61-rotate-list.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* &head, int k) {
+        //if the ll is empty return null
+        if(head==NULL)
+            return NULL;
+        
+        //if the ll has only one node, it does not need to be rotated.
+        if(head->next==NULL){
+            return head;
+        }
+        
+        ListNode* temp=head;
+        int len=0;
+        while(temp)
+        {
+            temp=temp->next;
+            len++;
+        }
+        
+        if(len<k)
+        {
+        k=k%len;
+        }
+        
+        if(k>0)
+        {
+        ListNode* curr = head;
+        ListNode* tail = head;
+        
+        //rotate it once
+        //reach the last node with tail pointer and the last second node with curr pointer
+        while(tail->next!=NULL)
+        {
+            curr=tail;
+            tail=tail->next;
+        }
+        curr->next=NULL;
+        tail->next=head;
+        head=tail;
+        tail=curr;
+        }
+        if(k>1)
+        {
+        //decrement before recurrsion so that it rotates k-1 times since we rotated it                       manually once.
+        k--;
+        rotateRight(head,k);
+        }
+            return head;
+    }
+};
