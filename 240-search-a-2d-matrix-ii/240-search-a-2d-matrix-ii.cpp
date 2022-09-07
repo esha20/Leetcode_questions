8a6f964655be0b4d6613c78b07b346d4d406d06e
Esha Mitra <72601896+esha20@users.noreply.github.com>
2022-09-07 17:34:20 +0530

    Time: 142 ms (79.91%), Space: 14.9 MB (21.53%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -5,18 +5,26 @@ public:
         int col= matrix[0].size();
         int row_ind= 0;
         int col_ind = col-1;
+        
         while(row_ind<row && col_ind>=0){
             int ele = matrix[row_ind][col_ind];
+            //starting from the last element of the first row.
+            
+            
             if(target==ele)
             {
                 return true;
             }
+            
+            
             if(target<ele)
             {
+                //target smaller, not in same col since the columns are sorted but can be in same row.
                 col_ind--;
             }
             if(target>ele)
             {
+                //target larger, can be in the same col but not in the same row since they are sorted.
                 row_ind++;
             }
         }
