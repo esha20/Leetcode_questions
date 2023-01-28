416d777dbcf337e9c7f5d1a248a4b734051cb1dd
Esha Mitra <72601896+esha20@users.noreply.github.com>
2023-01-28 23:37:07 +0530

    Time: 28 ms (42.13%), Space: 17.3 MB (23.37%) - LeetHub

--- 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
+++ 0116-populating-next-right-pointers-in-each-node/0116-populating-next-right-pointers-in-each-node.cpp
@@ -18,24 +18,31 @@ public:
 
 class Solution {
 public:
+    //read question carefully, it says in the first test case that the output is in level order. 
+    //so we use the level order approach. even if the hint was not give, we can see that the nodes are traversed        horizontally level wise so use the level order traversal
     Node* connect(Node* root) {
         if(root==NULL)
             return NULL;
         
         queue <Node*> q ;
+        //using the separator logic. we use null as separator to use after every level.
         q.push(root);
         q.push(NULL);
         
         while(!q.empty()){
+            //store the frontnode of the queue
             Node* frontnode = q.front();
             q.pop();
             
+            //if the frontnode is null, check for the next queue. 
+            //if queue is not empty, agle level k liye null dal do
             if(frontnode==NULL){
                // q.pop();
                 if(!q.empty())
                     q.push(NULL);
             }
             else{
+                //agar null nahi hai abhi ka frontnode toh uska left aur right queue mei add kar do.
                 frontnode->next=q.front();
                 if(frontnode->left)      q.push(frontnode->left);
                 if(frontnode->right)     q.push(frontnode->right);
