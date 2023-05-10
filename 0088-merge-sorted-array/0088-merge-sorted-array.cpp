fe8874c54def850148f649a8a67160d1a4140fce
Esha Mitra <72601896+esha20@users.noreply.github.com>
2023-05-10 15:13:08 +0530

    Time: 3 ms (63.13%), Space: 9.1 MB (31.46%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,33 +1,52 @@
 class Solution {
 public:
+    // void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    //     vector<int>ans;
+    //     int i=0,j=0;
+    //     while(i<m && j<n){
+    //         if(nums1[i]<nums2[j]){
+    //             ans.push_back(nums1[i]);
+    //             i++;
+    //         }
+    //         else{
+    //             ans.push_back(nums2[j]);
+    //             j++;
+    //         }
+    //     }
+    //     if(i<m){
+    //         while(i<m){
+    //             ans.push_back(nums1[i]);
+    //             i++;
+    //         }
+    //     }
+    //     if(j<n){
+    //     while(j<n){
+    //             ans.push_back(nums2[j]);
+    //             j++;
+    //         }
+    //     }
+    //     // nums1.clear();
+    //     for(int i=0;i<ans.size();i++){
+    //         nums1[i] = ans[i];
+    //     }
+    // }
+    
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int>ans;
-        int i=0,j=0;
-        while(i<m && j<n){
+        int i=m-1,j=n-1,k=m+n-1;
+        while(i>=0 && j>=0){
             if(nums1[i]<nums2[j]){
-                ans.push_back(nums1[i]);
-                i++;
+                nums1[k]=nums2[j];
+                k--;    j--;
             }
             else{
-                ans.push_back(nums2[j]);
-                j++;
+                nums1[k]=nums1[i];
+                k--;    i--;
             }
         }
-        if(i<m){
-            while(i<m){
-                ans.push_back(nums1[i]);
-                i++;
-            }
-        }
-        if(j<n){
-        while(j<n){
-                ans.push_back(nums2[j]);
-                j++;
-            }
-        }
-        // nums1.clear();
-        for(int i=0;i<ans.size();i++){
-            nums1[i] = ans[i];
+        while(j>=0){
+            nums1[k] = nums2[j];
+            k--;
+            j--;
         }
     }
 };
