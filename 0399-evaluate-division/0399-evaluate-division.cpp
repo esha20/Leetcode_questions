ecbc49f7a8fef07bdfa66e2e8ae11496208fea42
Esha Mitra <72601896+esha20@users.noreply.github.com>
2023-05-20 23:13:10 +0530

    Time: 3 ms (57.42%), Space: 8.4 MB (50.69%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -8,7 +8,10 @@ public:
             string u = equations[i][0];
             string v = equations[i][1];
             
+            //visualise a graph from a to b meaning a/b is 2 therefor the edge from a to b is 2.0
             adj[u][v] = values[i];
+            
+            //similarly for graph from b to a , the value will be 1/value
             adj[v][u] = 1.0/values[i];
         }
         return adj;
@@ -30,14 +33,20 @@ public:
     
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         
-        vector<double> res;
+        vector<double> res; //to store the final ans
+        
+        //build graph with the vectors provided.
         unordered_map <string, unordered_map<string,double>> adj = buildgraph(equations, values);
         
+        //formulate the ans
         for(auto i : queries){
             string u = i[0];
             string v = i[1];
             
+            //if the equations vector does not contain any string from the query vector, the ans is -1.0
             if(adj.find(u) == adj.end() || adj.find(v) == adj.end())    res.push_back(-1.0);
+            
+            //for every valid string, dfs is performed and for that we need a new  visited set everytime.
             else{
                 unordered_set<string> vis;
                 double temp = 1.0, ans = -1.0;
